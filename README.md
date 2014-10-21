9a73d4bcc2c5a513d9793bcac8264895b522fb18
Matt McFadyen <mmcfadyen91@gmail.com>
2014-10-20 23:36:05 -0400

    next bus API research :hatching_chick:

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## Displaying information on the following:
 * Weather analytics
-	* http://www.wunderground.com/weather/api/d/docs?d=data/index
+	* [Wunderground](http://www.wunderground.com/weather/api/d/docs?d=data/index)
 * TTC Tracking
+	* [TTC Real-Time Next Vehicle Arrival](http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=4427790e6f21d210VgnVCM1000003dd60f89RCRD&vgnextchannel=7807e03bb8d1e310VgnVCM10000071d60f89RCRD)
+		* [Docs](http://www.nextbus.com/xmlFeedDocs/NextBusXMLFeed.pdf)
+		* [TTC Routes](http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc)
+		* [Example Prediction](http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=14697&routeTag=37)
 * NFL & NHL schedule & scores
 * Yahoo Fantasy hockey API?
 * Quote gen
---
+++ ttc.js
@@ -0,0 +1,23 @@
+(function(){
+
+	// Route List
+	// http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc
+
+	// 48 Rathburn
+	// http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=48
+	
+		// 1470 East - CEDARLAND
+		// http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=1470&routeTag=48
+
+		// 14752 West - ROYAL YORK STATION
+		// http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=14752&routeTag=48
+
+	// 37 Islington
+	// http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=37
+
+		// 3828 South - ISLINGTON & RATHBURN
+		// http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=3828&routeTag=37
+
+		// 14697 North - ISLINGTON STATION
+		// http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=14697&routeTag=37
+})();
