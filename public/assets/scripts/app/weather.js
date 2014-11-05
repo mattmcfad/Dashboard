82c5119a0dad9fb426c920f505e1bc7a86465efd
Matt McFadyen <mmcfadyen91@gmail.com>
2014-11-04 21:30:49 -0500

    less ghetto hourly graph :whale2::ocean:

--- public/assets/scripts/app/weather.js
+++ public/assets/scripts/app/weather.js
@@ -1,8 +1,10 @@
 define(["global/utils", "app/apiKeys"], function(utils, keys) {
-	"use strict"
+	"use strict";
+	
 
 	var weatherCtrl = {};
 
+	
 	var baseUrl = "http://api.wunderground.com/api/",
 		endUrl  = "/q/Canada/Toronto.json";
 
@@ -34,7 +36,8 @@ define(["global/utils", "app/apiKeys"], function(utils, keys) {
 				}
 			}
 			console.log(hourlyForecast);
-			ghettoGraph(hourlyForecast);
+			//ghettoGraph(hourlyForecast);
+			graphLinePlot(hourlyForecast);
 
 		});
 		
@@ -43,6 +46,7 @@ define(["global/utils", "app/apiKeys"], function(utils, keys) {
 	function ghettoGraph(hour){
 
 		var graph = $("#graph").height(400);
+
 		for (var i = 0; i < 20; i+=2){
 			var height =  hour[i].temp + 0,
 				feel = hour[i].feel,
@@ -58,6 +62,60 @@ define(["global/utils", "app/apiKeys"], function(utils, keys) {
 		}
 	}
 
+	function graphLinePlot(hourlyForecast){
+		$("canvas").remove();
+		var graphContainer = $(".graph-container").append('<canvas id="canvas-graph" width="'+ ((window.innerWidth/2)-(63*2)) + '"></canvas>');
+
+		var ctx = $("#canvas-graph").get(0).getContext("2d");
+		
+		var temperatureData = [],
+			feelsLikeData = [],
+			hourOfDayData = [];
+		var j = 0;
+		for (var i = 0; i < hourlyForecast.length; i+=3){
+			var time = ("" + hourlyForecast[i].time.est).split(":00")
+			temperatureData[j] = hourlyForecast[i].temp*1;
+			feelsLikeData[j] = hourlyForecast[i].feel*1;
+			hourOfDayData[j] = time[0] + "" + time[1];
+			j++;
+		}
+
+		var data = {
+			labels: hourOfDayData,
+			datasets: [
+				{
+					label: "Temp",
+					fillColor: "rgba(220,220,220,0.2)",
+					strokeColor: "rgba(220,220,220,1)",
+					pointColor: "rgba(220,220,220,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "rgba(220,220,220,1)",
+					data: temperatureData
+				},
+				{
+					label: "Feels like",
+					fillColor: "rgba(151,187,205,0.2)",
+					strokeColor: "rgba(151,187,205,1)",
+					pointColor: "rgba(151,187,205,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "rgba(151,187,205,1)",
+					data: feelsLikeData
+				}
+			]
+		};
+	
+		new Chart(ctx).Line(data, {
+			scaleShowGridLines : true,
+			scaleGridLineColor : "rgba(0,0,0,.05)",
+			responsive : true
+		});
+
+
+		
+	}
+
 	weatherCtrl.printWeather = function(data) {
 		var list = $("#weather-details");
 		$.each(data.forecast.txt_forecast.forecastday, function(index, obj) {
@@ -66,6 +124,7 @@ define(["global/utils", "app/apiKeys"], function(utils, keys) {
 	}
 
 	weatherCtrl.init = function() {
+
 
 		$("#get-weather").on("click", function(e) {
 			e.preventDefault();
